2e8a78d78c52281db9a8b57659b61612e465ffcc
tarlanh <tarlan.hasanli@tudeng.ut.ee>
2019-04-20 11:38:12 +0300

    grouping Vetical Table SQL queries

---
+++ BSBM SQL Queries/src/main/scala/com/project/rdfbenchmarking/Queries/VerticalTableQueries.scala
@@ -0,0 +1,65 @@
+package com.project.rdfbenchmarking.Queries
+
+class VerticalTableQueries {
+
+  /*
+		 * Query 6: Find products having a label that contains a specific string.
+		 *
+		 * The consumer remembers parts of a product name from former searches.
+		 * He wants to find the product again by searching for the parts of the name that he remembers.
+		 * */
+
+  val query6 =
+    """
+      |
+    """.stripMargin
+
+  /*
+		 * Query 9: Get information about a reviewer.
+		 *
+		 * In order to decide whether to trust a review, the consumer asks for any
+		 * kind of information that is available about the reviewer.
+		 * */
+
+  val query9 =
+    """
+      |
+    """.stripMargin
+
+  /*
+		 * Query 10: Get offers for a given product which fulfill specific requirements.
+		 *
+		 * The consumer wants to buy from a vendor in the United States that is able to
+		 * deliver within 3 days and is looking for the cheapest offer that fulfills these requirements.
+		 * */
+
+  val query10 =
+    """
+      |
+    """.stripMargin
+
+  /*
+		 * Query 11: Get all information about an offer.
+		 *
+		 * After deciding on a specific offer, the consumer wants to get all information
+		 * that is directly related to this offer.
+		 * */
+
+  val query11 =
+    """
+      |
+    """.stripMargin
+
+  /*
+		 * Query 12: Export information about an offer into another schemata.
+		 *
+		 * After deciding on a specific offer, the consumer wants to save information
+		 * about this offer on his local machine using a different RDF schema.
+		 * */
+
+  val query12 =
+    """
+      |
+    """.stripMargin
+
+}
